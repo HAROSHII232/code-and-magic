a3d2add0d396b89d9505bf1089802dc2b1ec767b
HAROSHII <igorgrime@gmail.com>
2019-06-05 17:50:53 +0300

    Добавляет стату и диаграммы

---
+++ js/stat.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 200;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+
+var renderCloud = function(ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    return maxElement;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+    ctx.fillStyle = '#000';
+
+    var maxTime = getMaxElement(times);
+
+    // var playerIndex = 0;
+    // var playerName = 'Вы';
+
+    // var players = ['Вы', 'Иван', 'Юлия', 'Кекс'];
+
+    for (var i = 0; i < players.length; i++) {
+        ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    }
+
+    //     ctx.fillText(playerName, CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * playerIndex);
+    //     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * playerIndex, barWidth, BAR_HEIGHT);
+
+    //     var playerIndex = 1;
+    //     var playerName = 'Иван';
+
+    //     ctx.fillText(playerName, CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * playerIndex);
+    //     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * playerIndex, barWidth, BAR_HEIGHT);
+
+    //     var playerIndex = 2;
+    //     var playerName = 'Юлия';
+
+    //     ctx.fillText(playerName, CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * playerIndex);
+    //     ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * playerIndex, barWidth, BAR_HEIGHT);
+};
